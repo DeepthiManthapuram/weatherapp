9546dc5816915f3dc1aa9797bc295a92a44d4925
DeepthiManthapuram <deepthimanthapuram2005@gmail.com>
2025-07-13 22:27:45 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import requests
+
+api_key = "516a457989b1c01be9bc057f4fb1dc4c"
+
+user_input = input("Enter city: ")
+
+weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&appid={api_key}")
+
+if weather_data.json()['cod'] == '404':
+    print("No City Found")
+else:
+    weather = weather_data.json()['weather'][0]['main']
+    temp = round(weather_data.json()['main']['temp'])
+
+    print(f"The weather in {user_input} is: {weather}")
+    print(f"The temperature in {user_input} is: {temp}\u2109")
